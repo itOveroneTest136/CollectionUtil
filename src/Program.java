018e65021183a00b8c46f8a9cee72bca4f5eeb4a
andrei-kislushchanka <Andrei.Kislushchanka@sanofi.com>
2022-05-24 17:37:49 +0300

    Created a new method

--- src/Program.java
+++ src/Program.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+
 public class Program
 {
 	public static void main(String[] args)
 	{
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("текст один два");
+		arrayList.add("три текст четыре");
+		arrayList.add("пять шесть семь");
+		System.out.println(getElementsByWord(arrayList, "текст"));
+	}
 
+	public static ArrayList<String> getElementsByWord(ArrayList<String> arrayList, String word){
+		return (ArrayList<String>) arrayList.stream().filter(a -> a.toLowerCase().contains(word.toLowerCase())).collect(Collectors.toList());
 	}
 }
